e83453eae92fb829421f789a7215759164b9b835
farisde <farisde@maestralsolutions.com>
2021-09-29 17:18:35 +0200

    login and register fixed

--- src/store/auth-actions.js
+++ src/store/auth-actions.js
@@ -4,39 +4,52 @@ import { authActions } from "./auth-slice";
 export const sendRegisterUserRequest = (name, email, password) => {
   return async (dispatch) => {
     const registerUserRequest = async () => {
-      const response = await fetch("https://localhost:5001/Auth/Register", {
-        method: "POST",
-        body: JSON.stringify({
-          name,
-          email,
-          password,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+      const response = await fetch(
+        process.env.REACT_APP_API_URL + "api/auth/register",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            name,
+            email,
+            password,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
       const content = await response.json();
+      const swalConfig = {
+        title: "<div style='color:whitesmoke'>Sign Up error</div>",
+        html: `<div style='color:whitesmoke'>${content.message}</div>`,
+        icon: "error",
+        backdrop: true,
+        showConfirmButton: true,
+        confirmButtonColor: "#eb0028",
+        focusConfirm: false,
+        background: "#2C2C2C",
+      };
 
       if (!response.ok) {
-        Swal.fire({
-          title: "<div style='color:whitesmoke'>Sign Up error</div>",
-          html: `<div style='color:whitesmoke'>${content.message}</div>`,
-          icon: "error",
-          backdrop: true,
-          showConfirmButton: true,
-          confirmButtonColor: "#eb0028",
-          focusConfirm: false,
-          background: "#2C2C2C",
-        });
+        Swal.fire(swalConfig);
         return;
       }
+
+      Swal.fire({
+        ...swalConfig,
+        title: "<div style='color:whitesmoke'>Successful Sign Up</div>",
+        html: `<div style='color:whitesmoke'>You have successfully signed up for a MovieBuff account!
+        Now you can sign in to your account using your email and password.</div>`,
+        icon: "success",
+      });
 
       return content.data;
     };
 
     try {
       const userId = await registerUserRequest();
+      console.log(userId);
     } catch (error) {}
   };
 };
@@ -44,13 +57,16 @@ export const sendRegisterUserRequest = (name, email, password) => {
 export const sendLoginUserRequest = (email, password) => {
   return async (dispatch) => {
     const loginUserRequest = async () => {
-      const response = await fetch("https://localhost:5001/Auth/Login", {
-        method: "POST",
-        body: JSON.stringify({ email, password }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+      const response = await fetch(
+        process.env.REACT_APP_API_URL + "api/auth/login",
+        {
+          method: "POST",
+          body: JSON.stringify({ email, password }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
       const content = await response.json();
 
